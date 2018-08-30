18293bcc688d93e1ac9c7e83eb7011feac018f32
lonelyjourney <983621131@qq.com>
2018-08-30 17:02:12 +0800

    repair some bugs

--- app/src/main/java/com/example/admin/data_analysis_tool/Utils/TxtReader.java
+++ app/src/main/java/com/example/admin/data_analysis_tool/Utils/TxtReader.java
@@ -13,25 +13,19 @@ import java.util.List;
 //本程序用于读取矩阵型的记录数据，并转换为List<List<String>>格式数据
 public class TxtReader {
 
-    public List<List<String>> getRecord(InputStream stream) {
+    public List<List<String>> getRecord(String filepath) {
         List<List<String>> record = new ArrayList<List<String>>();
 
         try {
             String encoding = "UTF-8"; // 字符编码(可解决中文乱码问题 )
+            InputStream stream = new FileInputStream(new File(filepath));
             InputStreamReader read = new InputStreamReader(stream, encoding);
             BufferedReader bufferedReader = new BufferedReader(read);
             String lineTXT = null;
             while ((lineTXT = bufferedReader.readLine()) != null) {//读一行文件
-                String[] lineString = lineTXT.split("	");
+                String[] lineString = lineTXT.split(" ");
                 List<String> lineList = new ArrayList<String>();
-                for (int i = 0; i < lineString.length; i++) {//处理矩阵中的T、F、YES、NO
-//                    if (lineString[i].endsWith("T")
-//                            || lineString[i].endsWith("YES"))
-//                        lineList.add(record.get(0).get(i));
-//                    else if (lineString[i].endsWith("F")
-//                            || lineString[i].endsWith("NO"))
-                        ;// F，NO记录不保存
-//                    else
+                for (int i = 0; i < lineString.length; i++) {
                         lineList.add(lineString[i]);
                 }
                 record.add(lineList);
